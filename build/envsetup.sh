b86c91d47faf4402cbcf832fc977578a7fd52d7d
Paul Keith <javelinanddart@gmail.com>
2017-11-02 20:31:33 +0100

    build: Make eat use the latest zip in $OUT
    * Fixes weird issues with timestamp differences
    
    Change-Id: Ibeb0c5043b646faab6ce372290acf7efb4f22e15
    
    # Conflicts:
    #	build/envsetup.sh

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -80,9 +80,7 @@ alias bib=breakfast
 function eat()
 {
     if [ "$OUT" ] ; then
-        MODVERSION=$(get_build_var CM_VERSION)
-        ZIPFILE=$MODVERSION.zip
-        ZIPPATH=$OUT/$ZIPFILE
+        ZIPPATH=`ls -tr "$OUT"/lineage-*.zip | tail -1`
         if [ ! -f $ZIPPATH ] ; then
             echo "Nothing to eat"
             return 1
